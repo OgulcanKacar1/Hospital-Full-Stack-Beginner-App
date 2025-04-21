48743f7fa29a206b3c9bf5015e091fdf70be14c3
Ogulcan Kacar <ogulcankacarr@gmail.com>
2025-04-21 15:23:56 +0300

    Home Controller ile indexe erisim saglama.

--- src/main/java/com/java/hospital/hafta9/controllers/HomeController.java
+++ src/main/java/com/java/hospital/hafta9/controllers/HomeController.java
@@ -1,12 +1,33 @@
 package com.java.hospital.hafta9.controllers;
 
+import com.java.hospital.hafta9.models.Doctor;
+import com.java.hospital.hafta9.repositories.DoctorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+    public HomeController(DoctorRepository doctorRepository) {
+        this.doctorRepository = doctorRepository;
+    }
+
     @GetMapping("/")
     public String home() {
         return "index";
     }
+
+    @GetMapping("/{doctor}")
+    public String home(Model model){
+        Doctor doctor = new Doctor();
+//        doctor = doctorRepository.findById(1);
+        model.addAttribute("doctor", doctor);
+        return "index";
+    }
+
+
 }
