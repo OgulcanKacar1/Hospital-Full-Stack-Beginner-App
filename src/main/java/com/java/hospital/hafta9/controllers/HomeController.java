73323be1075ede57c1e1e6d67b24ece8bc2e6c54
Ogulcan Kacar <ogulcankacarr@gmail.com>
2025-04-21 14:41:04 +0300

    home controller eklenmesi

---
+++ src/main/java/com/java/hospital/hafta9/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.java.hospital.hafta9.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
